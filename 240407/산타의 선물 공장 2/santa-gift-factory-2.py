250ab964f05cb64f8d34861ef4ec4ffe8fce9b1b
김동연 <yoyo0704@naver.com>
2024-04-07 23:26:52 +0900

    Title: 산타의 선물 공장 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240407/산타의 선물 공장 2/santa-gift-factory-2.py
+++ 240407/산타의 선물 공장 2/santa-gift-factory-2.py
@@ -12,7 +12,7 @@
 
 각 벨트들은 맨앞 맨뒤 선물번호와 몇개 들고 있는지만 알고 있으면 됨.
 '''
-import math
+
 
 '''
 제한조건
@@ -27,118 +27,100 @@ import math
 각 선물 = [[앞번호, 뒷번호] . . . ] 앞번호 뒷번호가 없으면 내가 맨 처음이거나 마지막인 경우, 이 경우는 -1 로 표현
 '''
 
+from collections import deque
+
 def make_factory(n,m,p_list):
     global b_num_list, p_num_list
 
     for _ in range(n):
-        b_num_list.append([0,-1,-1])
+        b_num_list.append(deque([]))
     for _ in range(m):
         p_num_list.append([-1,-1])
     for idx,b_num in enumerate(p_list):
         p_num = idx + 1
-        if b_num_list[b_num][0] == 0:
-            b_num_list[b_num] = [1,p_num,p_num]
-        elif b_num_list[b_num][0] >= 1:
-            b_num_list[b_num][0] += 1
-            p_num_list[b_num_list[b_num][2]][1] = p_num
-            p_num_list[p_num][0] = b_num_list[b_num][2]
-            b_num_list[b_num][2] = p_num
+        if len(b_num_list[b_num]) == 0:
+            b_num_list[b_num].append(p_num)
+        elif len(b_num_list[b_num]) >= 1:
+            p_num_list[p_num][0] = b_num_list[b_num][-1]
+            p_num_list[b_num_list[b_num][-1]][1] = p_num
+            b_num_list[b_num].append(p_num)
 
 def move_all(m_src,m_dst):
     global b_num_list, p_num_list
 
-    src_cnt = b_num_list[m_src][0]
-    dst_cnt = b_num_list[m_dst][0]
-    if src_cnt != 0:
-        if dst_cnt == 0:
+    if len(b_num_list[m_src]) != 0:
+        if len(b_num_list[m_dst]) == 0:
             b_num_list[m_dst] = b_num_list[m_src]
-            b_num_list[m_src] = [0, -1, -1]
+            b_num_list[m_src] = deque([])
         else:
-            new_start = b_num_list[m_src][1]
-            src_end = b_num_list[m_src][2]
-            b_num_list[m_src] = [0,-1,-1]
-            b_num_list[m_dst][0] += src_cnt
-            dst_start = b_num_list[m_dst][1]
-            b_num_list[m_dst][1] = new_start
-            p_num_list[src_end][1] = dst_start
-            p_num_list[dst_start][0] = src_end
-
-    print(b_num_list[m_dst][0])
+            p_num_list[b_num_list[m_src][-1]][1] = b_num_list[m_dst][0]
+            p_num_list[b_num_list[m_dst][0]][0] = b_num_list[m_src][-1]
+            b_num_list[m_dst] = b_num_list[m_src] + b_num_list[m_dst]
+            b_num_list[m_src] = deque([])
+    print(len(b_num_list[m_dst]))
 
 def move_front(m_src,m_dst): ## 3가지 케이스 다 테스트 돌려봐야함
     global b_num_list, p_num_list
 
-    if b_num_list[m_src][0] == 0 and b_num_list[m_dst][0] == 0:
+    if len(b_num_list[m_src]) == 0 and len(b_num_list[m_dst]) == 0:
         pass
-    elif b_num_list[m_src][0] == 0:
-        dst_start = b_num_list[m_dst][1]
-        dst_second = p_num_list[dst_start][1]
-        b_num_list[m_src] = [1,dst_start,dst_start]
-        p_num_list[dst_start] = [-1,-1]
-        b_num_list[m_dst][0] -= 1
-        if b_num_list[m_dst][0] == 0:
-            b_num_list[m_dst][1] = -1
-            b_num_list[m_dst][2] = -1
+    elif len(b_num_list[m_src]) == 0:
+        if len(b_num_list[m_dst]) == 1:
+            b_num_list[m_src] = b_num_list[m_dst]
+            b_num_list[m_dst] = []
         else:
-            b_num_list[m_dst][1] = dst_second
-            p_num_list[dst_second][0] = -1
-    elif b_num_list[m_dst][0] == 0:
-        src_start = b_num_list[m_src][1]
-        src_second = p_num_list[src_start][1]
-        b_num_list[m_dst] = [1,src_start,src_start]
-        p_num_list[src_start] = [-1,-1]
-        b_num_list[m_src][0] -= 1
-        if b_num_list[m_src][0] == 0:
-            b_num_list[m_src][1] = -1
-            b_num_list[m_src][2] = -1
+            dst_start = b_num_list[m_dst].popleft()
+            b_num_list[m_src].appendleft(dst_start)
+            p_num_list[dst_start] = [-1,-1]
+            p_num_list[b_num_list[m_dst][0]][0] = -1
+    elif len(b_num_list[m_dst]) == 0:
+        if len(b_num_list[m_src]) == 1:
+            b_num_list[m_dst] = b_num_list[m_src]
+            b_num_list[m_src] = []
         else:
-            b_num_list[m_src][1] = src_second
-            p_num_list[src_second][0] = -1
+            src_start = b_num_list[m_src].popleft()
+            b_num_list[m_dst].appendleft(src_start)
+            p_num_list[src_start] = [-1,-1]
+            p_num_list[b_num_list[m_src][0]][0] = -1
     else: #테스트 완료
-        src_start = b_num_list[m_src][1]
-        src_second = p_num_list[src_start][1]
-        dst_start = b_num_list[m_dst][1]
-        dst_second = p_num_list[dst_start][1]
-        b_num_list[m_src][1] = dst_start
-        b_num_list[m_dst][1] = src_start
-        p_num_list[src_start][1] = dst_second
-        p_num_list[dst_second][0] = src_start
-        p_num_list[dst_start][1] = src_second
-        p_num_list[src_second][0] = dst_start
-        if b_num_list[m_src][0] == 1:
-            b_num_list[m_src][2] = dst_start
-        if b_num_list[m_dst][0] == 1:
-            b_num_list[m_dst][2] = src_start
+        src_start = b_num_list[m_src].popleft()
+        dst_start = b_num_list[m_dst].popleft()
+        b_num_list[m_dst].appendleft(src_start)
+        b_num_list[m_src].appendleft(dst_start)
+        if len(b_num_list[m_dst]) == 1:
+            p_num_list[src_start] = [-1, -1]
+        else:
+            p_num_list[src_start][1] = b_num_list[m_dst][1]
+            p_num_list[b_num_list[m_dst][1]][0] = src_start
+        if len(b_num_list[m_src]) == 1:
+            p_num_list[dst_start] = [-1, -1]
+        else:
+            p_num_list[dst_start][1] = b_num_list[m_src][1]
+            p_num_list[b_num_list[m_src][1]][0] = dst_start
 
+    print(len(b_num_list[m_dst]))
 
-    print(b_num_list[m_dst][0])
-
-def move_half(m_src,m_dst): #옮기지 않으면 출력하지 않는가?? 문제 설명이 애매
+def move_half(m_src,m_dst):
     global b_num_list, p_num_list
 
-    n = b_num_list[m_src][0]
-    target = n//2
-    src_start = b_num_list[m_src][1]
-    dst_start = b_num_list[m_dst][1]
-    target_p_num = src_start
+    n = len(b_num_list[m_src])
+    cnt = n//2
+    half_q = deque([])
+    src_start = b_num_list[m_src][0]
+    half_end = src_start
     if n > 1:
-        for _ in range(target-1):
-            target_p_num = p_num_list[target_p_num][1]
-        new_src_start = p_num_list[target_p_num][1]
-        b_num_list[m_src][0] -= target
-        b_num_list[m_dst][0] += target
-        if dst_start != -1:
-            p_num_list[dst_start][0] = target_p_num
-        p_num_list[target_p_num][1] = dst_start
-        p_num_list[new_src_start][0] = -1
-        b_num_list[m_src][1] = new_src_start
-        if dst_start == -1:
-            b_num_list[m_dst][2] = target_p_num
-            b_num_list[m_dst][1] = src_start
+        for _ in range(cnt):
+            half_q.append(b_num_list[m_src].popleft())
+        p_num_list[b_num_list[m_src][0]][0] = -1
+        if len(b_num_list[m_dst]) == 0:
+            p_num_list[half_q[-1]][1] = -1
+            b_num_list[m_dst] = half_q
         else:
-            b_num_list[m_dst][1] = src_start
-        #print("s,t:",dst_start,src_start,target_p_num)
-    print(b_num_list[m_dst][0])
+            p_num_list[b_num_list[m_dst][0]][0] = half_q[-1]
+            p_num_list[half_q[-1]][1] = b_num_list[m_dst][0]
+            b_num_list[m_dst] = half_q + b_num_list[m_dst]
+
+    print(len(b_num_list[m_dst]))
 
 def p_num_info(p_num):
     global b_num_list, p_num_list
@@ -150,14 +132,16 @@ def p_num_info(p_num):
 def b_num_info(b_num):
     global b_num_list, p_num_list
 
-    a = b_num_list[b_num][1]
-    b = b_num_list[b_num][2]
-    c = b_num_list[b_num][0]
+    c = len(b_num_list[b_num])
+    if c == 0:
+        a,b = -1,-1
+    else:
+        a = b_num_list[b_num][0]
+        b = b_num_list[b_num][-1]
 
     print(a + 2*b + 3*c)
 
-
-b_num_list = [[]]
+b_num_list = [deque([])]
 p_num_list = [[]]
 q = int(input())
 for stage in range(1,q+1):
@@ -168,31 +152,29 @@ for stage in range(1,q+1):
         m = order[2]
         p_list = order[3:]
         make_factory(n, m, p_list)
-        #print(b_num_list)
-        #print(p_num_list)
+
     elif order[0] == 200:
         m_src = order[1]
         m_dst = order[2]
         move_all(m_src, m_dst)
-        #print(b_num_list)
-        #print(p_num_list)
+
     elif order[0] == 300:
         m_src = order[1]
         m_dst = order[2]
         move_front(m_src, m_dst)
-        #print(b_num_list)
-        #print(p_num_list)
+
     elif order[0] == 400:
         m_src = order[1]
         m_dst = order[2]
         move_half(m_src, m_dst)
-        #print(b_num_list)
-        #print(p_num_list)
+
     elif order[0] == 500:
         p_num = order[1]
         p_num_info(p_num)
+
     elif order[0] == 600:
         b_num = order[1]
         b_num_info(b_num)
+
     # print(b_num_list)
     # print(p_num_list)
