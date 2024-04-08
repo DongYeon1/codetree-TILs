6a625b488db022f8fbee12a03ec87420d174d012
김동연 <yoyo0704@naver.com>
2024-04-08 15:17:56 +0900

    Title: 토끼와 경주, Time: 9568ms, Memory: 28MB, Status: Time Limit Exceed - CodeTree

--- 240408/토끼와 경주/rabit-and-race.py
+++ 240408/토끼와 경주/rabit-and-race.py
@@ -7,65 +7,42 @@ id : [d,cnt,score] 형태
 
 '''
 from collections import defaultdict
+import heapq
 
 Q = int(input())
 input_list = list(map(int, input().split()))
 N = input_list[1]
 M = input_list[2]
 P = input_list[3]
-pid_dict = defaultdict(list)
+pq_select = []
+pid_d_dict = {}
+pid_score_dict = {}
+pid_rc_dict = {}
 for i in range(1, P + 1):
-    pid_dict[input_list[(i + 1) * 2]].append((1, 1))  # 좌표 0
-    pid_dict[input_list[(i + 1) * 2]].append(input_list[(i + 1) * 2 + 1])  # d 1
-    pid_dict[input_list[(i + 1) * 2]].append(0)  # cnt 2
-    pid_dict[input_list[(i + 1) * 2]].append(0)  # score 3
-
-pid_dict[0] = [(100001, 100001), float("inf"), float("inf"), 0]
-
-#print(pid_dict)
-
-# print(pid_dict)
-
-def select_moving_rabbit():
-    global pid_dict
-
-    fpid = 0
-    for tpid in pid_dict:
-        if pid_dict[tpid][2] < pid_dict[fpid][2]:
-            fpid = tpid
-        elif pid_dict[tpid][2] == pid_dict[fpid][2]:
-            if pid_dict[tpid][0][0] + pid_dict[tpid][0][1] < pid_dict[fpid][0][0] + pid_dict[fpid][0][1]:
-                fpid = tpid
-            elif pid_dict[tpid][0][0] + pid_dict[tpid][0][1] == pid_dict[fpid][0][0] + pid_dict[fpid][0][1]:
-                if pid_dict[tpid][0][0] < pid_dict[fpid][0][0]:
-                    fpid = tpid
-                elif pid_dict[tpid][0][0] == pid_dict[fpid][0][0]:
-                    if pid_dict[tpid][0][1] < pid_dict[fpid][0][1]:
-                        fpid = tpid
-                    elif pid_dict[tpid][0][1] == pid_dict[fpid][0][1]:
-                        if tpid < fpid:
-                            fpid = tpid
-    if fpid == 0:
-        raise
-
-    return fpid
-
+    pid = input_list[(i + 1) * 2]
+    pid_d = input_list[(i + 1) * 2 + 1]
+    heapq.heappush(pq_select,(0,2,1,1,pid))
+    pid_d_dict[pid] = pid_d
+    pid_score_dict[pid] = 0
+    pid_rc_dict[pid] = (0,0)
 
 def in_board(r, c):
     if 1 <= r <= N and 1 <= c <= M:
         return True
     return False
 
+def select_and_move_rabbit():
+    global pq_select
 
-def move_rabbit(pid):
-    global pid_dict
+    cnt, r_c, r, c, pid = heapq.heappop(pq_select)
+    d = pid_d_dict[pid]
+    score = pid_d_dict[pid]
 
-    point, d, cnt, score = pid_dict[pid]
     dr = [0, 1, 0, -1]
     dc = [1, 0, -1, 0]
     candidate_point = []
     for i in range(4):
-        tr, tc = point
+        tr,tc = r, c
         if i == 0 or i == 2:
             real_d = d % ((M-1)*2)
         elif i == 1 or i == 3:
@@ -88,20 +65,25 @@ def move_rabbit(pid):
             elif fr == tr:
                 if fc < tc:
                     fr, fc = tr, tc
-    for update_pid in pid_dict:
-        if update_pid == pid:
-            pid_dict[pid] = [(fr, fc), d, cnt + 1, score]
+
+    for tpid in pid_score_dict:
+        s = fr + fc
+        if tpid == pid:
+            heapq.heappush(pq_select,(cnt+1,fr + fc,fr,fc,tpid))
+            pid_rc_dict[tpid] = (fr,fc)
         else:
-            pid_dict[update_pid][3] += fr + fc
+            pid_score_dict[tpid] += s
+    #print("fr,fc:",fr,fc)
+    return pid
 
 
 def select_scoring_rabbit(moved_rabbit_set, S):
-    global pid_dict
+    global pq_select
 
     fr, fc = 0, 0
     fmr_pid = 0
     for mr_pid in moved_rabbit_set:
-        tr, tc = pid_dict[mr_pid][0]
+        tr, tc = pid_rc_dict[mr_pid]
         if fr + fc < tr + tc:
             fr, fc = tr, tc
             fmr_pid = mr_pid
@@ -116,29 +98,37 @@ def select_scoring_rabbit(moved_rabbit_set, S):
                 elif fc == tc:
                     if fmr_pid < mr_pid:
                         fmr_pid = mr_pid
-    pid_dict[fmr_pid][3] += S
+    pid_score_dict[fmr_pid] += S
 
 
 for _ in range(Q - 1):
     order = list(map(int, input().split()))
-    #print(order)
+    # print(order)
+    # print("--------------------")
     if order[0] == 200:
         K = order[1]
         S = order[2]
         moved_rabbit_set = set()
-        for _ in range(K):
-            mr_pid = select_moving_rabbit()
-            move_rabbit(mr_pid)
+        for k in range(K):
+            # print("round:",k+1)
+            mr_pid = select_and_move_rabbit()
             moved_rabbit_set.add(mr_pid)
-            #print(pid_dict)
+            # print(pid_d_dict)
+            # print(pid_score_dict)
+            # print(pid_rc_dict)
         select_scoring_rabbit(moved_rabbit_set, S)
-        #print(pid_dict)
+        # print(pid_d_dict)
+        # print(pid_score_dict)
+        # print(pid_rc_dict)
     elif order[0] == 300:
         pid = order[1]
         L = order[2]
-        pid_dict[pid][1] *= L
+        pid_d_dict[pid] *= L
     elif order[0] == 400:
         max_score = 0
-        for pid in pid_dict:
-            max_score = max(max_score, pid_dict[pid][3])
+        for pid in pid_score_dict:
+            max_score = max(max_score, pid_score_dict[pid])
         print(max_score)
+    # print(pid_d_dict)
+    # print(pid_score_dict)
+    # print(pid_rc_dict)
